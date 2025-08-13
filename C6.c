8a19213093536a2594c1baeaee10592a0bc6d2dc
krinameet <krinameet645@gmail.com>
2025-08-13 12:39:48 +0530

    Add files via upload

---
+++ C6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ float min,hr;
+ printf("enter time in hours");
+ scanf("%f",&hr);
+ min=hr*60;
+ printf("the time in minutes =%f\n",min);
+
+}
---
+++ C9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ float R,D;
+ printf("enter currency in rupees");
+ scanf("%f",&R);
+ D=R/45;
+ printf("the currency in dollars will be =%f\n",D);
+
+}
