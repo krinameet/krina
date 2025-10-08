a9b09e68ed57de0acc2eeb06cf2d796cf61681c4
krinameet <krinameet645@gmail.com>
2025-10-08 12:05:26 +0530

    Add files via upload

---
+++ built-in 8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[]="HELLO";
+if(strrev(str)==(str))
+{
+    printf("the given string is pallindrome");
+}
+else
+{
+  printf("the given string is not pallindrome");
+}
+}
