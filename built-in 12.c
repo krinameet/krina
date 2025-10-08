63d00b43d431841c2365d27aa4536effcee9b1f6
krinameet <krinameet645@gmail.com>
2025-10-08 12:50:34 +0530

    Add files via upload

---
+++ built-in 12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i, cnt=0;
+char str[]="hello";
+for(i=0;i<=str[i];i++)
+{
+  if (str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' || str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+  {
+    cnt++;
+  }
+  }
+  printf("%d",cnt);
+  }
---
+++ built-in 6.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1[]="abcd", str2[]="ABCD";
+printf("%d",strcmp(str1,str2));
+}
